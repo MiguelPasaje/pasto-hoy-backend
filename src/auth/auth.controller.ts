706e3720c77758da50cbe2f682be5bbeec3a4805
MiguelPasaje <cartel-dy@hotmail.com>
2023-08-28 21:55:17 -0500

    auth. ya se recive el token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    signIn(@Body() signinDto: Record<string, any>){
+        return this.authService.signIn(signinDto.username, signinDto.password);
+
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    constructor(
+        private userService: UsersService,
+        private jwtService: JwtService
+        ){}
+
+    async signIn(username: string, pass: string): Promise<any>{
+        const user = await this.userService.findOne(username);
+        if(user?.password !== pass){
+            throw new UnauthorizedException();
+        }
+
+        const payload = {sub: user.userId, username: user.name}
+
+        return {
+            acces_token: await this.jwtService.signAsync(payload)
+        }
+        
+        /* const { password, ...result} = user;
+        console.log(password);
+        
+        return result */
+    }
+
+}
